5e6d7b8ac3ca9e661b6e075c0d7ef26f0182bdb8
HaneeshSai <139609774+HaneeshSai@users.noreply.github.com>
2024-04-21 22:32:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const client = new Client({
     remotePath:
       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
+  puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }
 });
 
 client.on("ready", () => {
